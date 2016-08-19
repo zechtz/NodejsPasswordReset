e9e1909ddef4dea783ea3fa396f63f709e60190f
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-19 16:29:36 +0300

    Fix name error in session

---
+++ app/controllers/session.js
@@ -0,0 +1,28 @@
+var express        =  require('express');
+var passport       =  require('passport');
+var router         =  express.Router();
+var passportConfig =  require('../../config/passport')
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('sessions/new', { 
+      title : 'Login'
+    });
+});
+
+router.post('/session/create', function(req, res, next){
+  passport.authenticate('local-login', function(err, user, info){
+    if (err) return next(err);
+    if (!user) return res.redirect('/login');
+    req.login(user, function(err){
+      if (err) return next(err)
+        return res.redirect('/');
+    });
+  })(req, res, next)
+});
+
+router.post('/session/destroy', function(req, res){
+  req.logout();
+  res.rediret('/');
+});
+module.exports = router;
