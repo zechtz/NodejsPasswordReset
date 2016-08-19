e1f4bbe15734518478a01a8a3fe7e5c8c55656e3
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-19 16:33:33 +0300

    ignore bower components and npm-log file

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var session       =  require('express-session')
 var nodemailer    =  require('nodemailer');
-var passport      =  require('passport');
-var LocalStrategy =  require('passport-local').Strategy;
 var async         =  require('async');
 var express       =  require('express');
 var path          =  require('path');
@@ -10,14 +8,27 @@ var logger        =  require('morgan');
 var cookieParser  =  require('cookie-parser');
 var bodyParser    =  require('body-parser');
 var User          =  require('./app/models/user');
+var passport      =  require('passport');
+var ejs           =  require('ejs');
+var flash         =  require('express-flash');
+var engine        =  require('ejs-mate');
+var mongoose      =  require('mongoose');
+var mongodb       =  require('mongodb');
+var MongoStore    =  require('connect-mongo/es5')(session);
 
 var indexRoutes   =  require('./app/controllers/index');
 var usersRoutes   =  require('./app/controllers/users');
+var sessionRoutes =  require('./app/controllers/session');
 
 var app           =  express();
 
+//connect to mongo database
+var dbConfig      =  require('./config/db')
+mongoose.connect(dbConfig.url);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -26,11 +37,33 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'session secret key' }));
+app.use(session({
+   resave:              true,
+   saveUninitialized:   true,
+   secret:              dbConfig.secret,
+   store:               new MongoStore({ url: dbConfig.url, autoReconnect: true }) 
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* lets bring in our routes  */
 app.use('/', indexRoutes);
-app.use('/users', usersRoutes);
+/* app.use('/users', usersRoutes); */
+app.use(usersRoutes);
+app.use(sessionRoutes);
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*
+   this middleware is used to make the user object accessible
+   throughout the whole request / response cycle and it has to be
+   defined after app.use(passport.session()) otherwise it won't work
+*/
+app.use(function(req, res, next){
+  res.locals.user = req.user;
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
