247dc44d1a3e662df2684f8ee53e85d43cb862b3
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-19 12:34:54 +0300

    Add modules

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var express      =  require('express');
-var path         =  require('path');
-var favicon      =  require('serve-favicon');
-var logger       =  require('morgan');
-var cookieParser =  require('cookie-parser');
-var bodyParser   =  require('body-parser');
+var session       =  require('express-session')
+var mongoose      =  require('mongoose');
+var nodemailer    =  require('nodemailer');
+var passport      =  require('passport');
+var LocalStrategy =  require('passport-local').Strategy;
+var bcrypt        =  require('bcrypt-nodejs');
+var async         =  require('async');
+var crypto        =  require('crypto');
+var express       =  require('express');
+var path          =  require('path');
+var favicon       =  require('serve-favicon');
+var logger        =  require('morgan');
+var cookieParser  =  require('cookie-parser');
+var bodyParser    =  require('body-parser');
 
-var routes       =  require('./routes/index');
-var users        =  require('./routes/users');
+var routes        =  require('./routes/index');
+var users         =  require('./routes/users');
 
-var app          =  express();
+var app           =  express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +28,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'session secret key' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
