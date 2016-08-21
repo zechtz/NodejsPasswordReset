38652a4d1992d52348b066e1db397837115ebea6
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-21 12:14:35 +0300

    Add a sample secrets configuration file

---
+++ config/secrets.sample.js
@@ -0,0 +1,9 @@
+/* 
+   rename this file to secrets.js and then add 
+   your proper SendGrid/GMail credentials 
+*/
+
+module.exports = {
+  'username': 'yousendgrideusername',
+  'password': 'yoursendgridpassword'
+};
